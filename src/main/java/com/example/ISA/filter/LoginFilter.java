4191bb941768aa843e7bccbf470657b28632059a
suzuki_shota <suzuki.shota@alhinc.jp>
2025-06-16 13:10:57 +0900

    2025年6月16日　13時10分 ｊｓ追加

---
+++ src/main/java/com/example/ISA/filter/LoginFilter.java
@@ -0,0 +1,45 @@
+//package com.example.ISA.filter;
+//
+//import java.io.IOException;
+//
+//import jakarta.servlet.Filter;
+//import jakarta.servlet.FilterChain;
+//import jakarta.servlet.FilterConfig;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.ServletRequest;
+//import jakarta.servlet.ServletResponse;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import jakarta.servlet.http.HttpSession;
+//
+//import static com.example.ISA.constfolder.ErrorMessage.*;
+//public class LoginFilter implements Filter {
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response,
+//                         FilterChain chain) throws IOException, ServletException {
+//
+//        //ServletRequestをHttpServletRequestに型変更
+//        HttpServletRequest httpRequest = (HttpServletRequest) request;
+//        HttpServletResponse httpResponse = (HttpServletResponse) response;
+//
+//        //セッションの獲得→trueだとセッションがない場合は作ってくれる
+//        HttpSession session = httpRequest.getSession(true);
+//        //ログインユーザが存在していない場合→ログイン画面にエラーメッセージ表示
+//        if (session.getAttribute("loginUser") == null) {
+//            session.setAttribute("filterMessage", E0024);
+//            // ログイン画面に遷移
+//            httpResponse.sendRedirect("/login");
+//        } else {
+//            // 通常実行
+//            chain.doFilter(httpRequest, httpResponse);
+//        }
+//    }
+//
+//    @Override
+//    public void init(FilterConfig config) {
+//    }
+//
+//    @Override
+//    public void destroy() {
+//    }
+//}
